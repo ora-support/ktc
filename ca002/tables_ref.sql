2edfccf254694322a8e9b37a1839628dbd286dd2
swankham <s.wankham@gmail.com>
2017-09-15 10:55:03 +0700

    New file

---
+++ ca002/tables_ref.sql
@@ -0,0 +1,10 @@
+/* Formatted on 9/15/2017 10:19:54 AM (QP5 v5.256.13226.35538) */
+SELECT * FROM gl_ca_je_headers;
+
+SELECT * FROM gl_ca_je_batches;
+
+SELECT * FROM gl_ca_je_lines;
+
+SELECT * FROM gl_ca_je_line_match;
+
+SELECT * FROM gl_ca_je_match_lock;
